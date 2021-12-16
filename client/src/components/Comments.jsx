09778e9d21c88999716a954540564a411a7ba9b7
Zdenda <zdenda.rotal@seznam.cz>
2021-12-16 17:23:20 +0100

    opraveni chyb v ProfileSettings a v Share

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -4,6 +4,7 @@ import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import changePath from '../changePath';
 import { GlobalContext } from '../context/GlobalState';
 import Comment from './Comment';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Comments = ({post}) => {
     const {user, backgroundColor1, backgroundColor2} = useContext(GlobalContext);
@@ -19,6 +20,9 @@ const Comments = ({post}) => {
 
     // promenna ktera ma ulozene komentare s spravne vnoreneme komentarema v children
     const [allComments, setAllComments] = useState([]);
+
+    // promenna zda uzivatel kliknul na talickto pridej komentar a stranka se nacita
+    const [ifLoading, setifLoading] = useState(false);
 
     useEffect(() => {
         // najde vsechny komentare vztazene k nasemu prispevku
@@ -58,6 +62,7 @@ const Comments = ({post}) => {
     // prida komentar
     const addComment = async (valueOfInput, idOfparentComment, idOfUser) => {
         try {
+          setifLoading(true);
           const newComment = {
             value: valueOfInput,
             idOfPost: post._id,
@@ -69,6 +74,7 @@ const Comments = ({post}) => {
           // dostaneme vsechny komentare vztazene k prispevku
           const newComments = await axios.get(changePath(`/comments/getComments/${post._id}`));
           setComments(newComments.data);
+          setifLoading(false);
           setValueOfInput("");
           return true
         } catch (err) {
@@ -85,7 +91,7 @@ const Comments = ({post}) => {
                 <div className="commentsContainer">
                     <div className="addComment">
                       <input className="addCommentInput" style={{backgroundColor: backgroundColor2}} type="text" value={valueOfInput} onChange={(e) => setValueOfInput(e.target.value)}placeholder="co máš na mysli..." />
-                      <button className="addCommentButton" style={{backgroundColor: backgroundColor1}} onClick={() => addComment(valueOfInput, null, user._id)}>přidej komentář</button>
+                      <button className="addCommentButton" style={{backgroundColor: backgroundColor1}} onClick={() => addComment(valueOfInput, null, user._id)}>{!ifLoading ? "přidej komentář" : <ClipLoader color={backgroundColor2} size={10} />}</button>
                     </div>
                         {   
                           allComments.map(comment => (
