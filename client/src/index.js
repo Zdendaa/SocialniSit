1aa1ace13d650a020681971dc651b254951c0d40
Zdenda <zdenda.rotal@seznam.cz>
2022-06-04 13:36:52 +0200

    cordova config start

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalProvider } from './context/GlobalState';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const startApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (window.cordova) {
+  document.addEventListener("deviceready", startApp, false);
+} else {
+  startApp();
+}
+
+
