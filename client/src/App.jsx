f6e48ccd45bf34acf162b154a40f166cb399eda5
Zdendaa <zdenda.rotal@seznam.cz>
2022-06-09 09:08:39 +0200

    uprava url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   useEffect(() => {
     // pripojeni socket.io
     if (user) {
-      setSocket(io("ws://https://socialappsocrot.herokuapp.com/:8900"));
+      setSocket(io("https://socialappsocrot.herokuapp.com/:8900"));
     }
   }, [user])
 
