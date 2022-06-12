a3a61d0cee65ea6663e5063ec1616c1d7c1498dc
Zdenda <zdenda.rotal@seznam.cz>
2022-06-12 19:02:20 +0200

    nwm

--- api/index.js
+++ api/index.js
@@ -49,7 +49,7 @@ const __dirname1 = path.resolve(); // current path
 if (process.env.NODE_ENV = "production") {
     app.use(express.static(path.join(__dirname1, "../client/build")));
 
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'));
     });
 } else {
