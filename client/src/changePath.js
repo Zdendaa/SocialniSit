0776c6b57bdc2650d589f1a68d704fb33204e896
Zdenda <zdenda.rotal@seznam.cz>
2023-07-30 14:11:19 +0200

    nejake zmeny

--- client/src/changePath.js
+++ client/src/changePath.js
@@ -1,5 +1,5 @@
 const changePath = (path) => {
-    return path; // tato funkce slouzi k zmene cesty
+    return `https://rotal-socialnisit.onrender.com/api${path}`; // tato funkce slouzi k zmene cesty
 }
 
 export default changePath;
